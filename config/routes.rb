1d34a81d79fc5d420e682866b13a24b9f16d2a30
Diego Aquilina <diegoaquilina@hotmail.com>
2022-08-23 17:34:25 -0300

    parte 1 controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,28 @@
 Rails.application.routes.draw do
-  # root to: "pages#home"
+  root to: "locations#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/market', to: 'pages#market', as: 'market'
-  get '/hotel/busca', to 'locations#index'
-  get '/user/sign_up', to 'devices#registration'
-  get '/user/sign_in', to 'devices#registration'
-  get '/pets/new', to 'pets#new'
-  post '/pets', to 'pets#create'
-  get '/reservations/new', to 'reservations#reservations#new'
-  get '/guests', to 'reservations#guests'
-  patch '/updatereservation', to 'reservations#updatereservations'
-  delete 'reservations/:id', to 'reservations#destroy'
-  get '/hosts/sign_up', to 'hosts#hosts#new'
-  get 'reservation/:id', to 'reservations#reservations#show'
-  get '/reservation/new', to 'reservations#reservations#new'
-  get '/reservations/:id/edit', to 'reservations#reservations#edit'
-  patch '/reservations/:id', to 'reservations#reservations#update'
-  get '/location/:id/edit', to 'locations#locations#edit'
-  patch '/location/:id', to 'locations#locations#update'
+
+  resources :locations, only: [:index, :show]
+
+
+  #get '/market', to: 'pages#market', as: 'market'
+  #get '/hotel/busca', to 'locations#index'
+  #get '/user/sign_up', to 'devices#registration'
+  #get '/user/sign_in', to 'devices#registration'
+  #get '/pets/new', to 'pets#new'
+  #post '/pets', to 'pets#create'
+  #get '/reservations/new', to 'reservations#reservations#new'
+  #get '/guests', to 'reservations#guests'
+  #patch '/updatereservation', to 'reservations#updatereservations'
+  #delete 'reservations/:id', to 'reservations#destroy'
+  #get '/hosts/sign_up', to 'hosts#hosts#new'
+  #get 'reservation/:id', to 'reservations#reservations#show'
+  #get '/reservation/new', to 'reservations#reservations#new'
+  #get '/reservations/:id/edit', to 'reservations#reservations#edit'
+  #patch '/reservations/:id', to 'reservations#reservations#update'
+  #get '/location/:id/edit', to 'locations#locations#edit'
+  #patch '/location/:id', to 'locations#locations#update'
 end
