11f5608880d24531a678fc9f486e694d8d9eec9b
Diego Aquilina <diegoaquilina@hotmail.com>
2022-08-24 11:49:11 -0300

    home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: "locations#index"
+  root to: "pages#market"
   devise_for :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
