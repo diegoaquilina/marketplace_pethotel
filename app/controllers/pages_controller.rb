e2ada069fe0c0bc2b6b76c65595caaa6636eec8f
felipe siqueira <felipedesouzasiqueira@gmail.com>
2022-08-26 13:01:18 -0300

    aut profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,17 @@ class PagesController < ApplicationController
     @locations = Location.all
   end
 
+  # def show
+  #   @user = User.find(params[:id])
+  #   @pets = Pet.where(user: @user)
+  #   @locations = Location.where(user: @user)
+  # end
+
   def profile
     @user = current_user
     @pets = Pet.where(user: @user)
+    @locations = Location.where(user: @user)
   end
+
+
 end
